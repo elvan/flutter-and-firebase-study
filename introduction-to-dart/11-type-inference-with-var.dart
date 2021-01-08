fa2a92a35a24238bf5e755d5f31fdfe1fdad1def
Elvan H <elvanhd@gmail.com>
2021-01-08 16:12:21 +0700

    type-inference-with-var

---
+++ introduction-to-dart/11-type-inference-with-var.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = 'Elvan';
+  var age = 37;
+  var height = 1.70;
+
+  print("Hello, I'm $name");
+  print("My name has ${name.length} letters");
+  print("I'm $age years old");
+  print("I'm $height meters tall");
+}
