32bd4750201f65d5330de13642a8fc9544560e62
Elvan H <elvanhd@gmail.com>
2021-01-13 00:10:57 +0700

    adding a column layout

--- time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
@@ -8,6 +8,20 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.orange,
+              child: SizedBox(
+                width: 100.0,
+                height: 100.0,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
