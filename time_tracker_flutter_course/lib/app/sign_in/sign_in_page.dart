04cc0cc318df72031973ed48e9e65d2e6264e4db
Elvan H <elvanhd@gmail.com>
2021-01-13 00:29:38 +0700

    adding some boxes

--- time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
@@ -8,19 +8,35 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.orange,
-              child: SizedBox(
-                height: 100.0,
-              ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100.0,
             ),
-          ],
-        ),
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+        ],
       ),
     );
   }
