bb7ff4bfb0f18d275d40031576541aa319dc9495
Elvan H <elvanhd@gmail.com>
2021-01-13 22:56:26 +0700

    creating the AuthBase abstract class

--- time_tracker_flutter_course/lib/app/auth/auth_service.dart
+++ time_tracker_flutter_course/lib/app/auth/auth_service.dart
@@ -1,15 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthService {
+import 'auth_base.dart';
+
+class AuthService implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
 
+  @override
   User get currentUser => _firebaseAuth.currentUser;
 
+  @override
   Future<User> signInAnonymously() async {
     final userCredential = await _firebaseAuth.signInAnonymously();
     return userCredential.user;
   }
 
+  @override
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
