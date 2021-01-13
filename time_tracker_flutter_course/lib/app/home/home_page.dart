557c5ca92a11b69f1d4bdafb7d3d9ab63459da29
Elvan H <elvanhd@gmail.com>
2021-01-13 22:26:48 +0700

    adding the sign out functionality

--- time_tracker_flutter_course/lib/app/home/home_page.dart
+++ time_tracker_flutter_course/lib/app/home/home_page.dart
@@ -1,24 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final VoidCallback onSignOut;
+
+  const HomePage({Key key, @required this.onSignOut}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-        actions: [
-          FlatButton(
-            onPressed: () {},
-            child: Text(
-              'Sign Out',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-              ),
+      appBar: _buildAppBar(),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: Text('Home Page'),
+      actions: [
+        FlatButton(
+          onPressed: _signOut,
+          child: Text(
+            'Sign Out',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
+  }
+
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      onSignOut();
+    } on Exception catch (e) {
+      print(e.toString());
+    }
   }
 }
