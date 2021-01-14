0b2e3904d2a533b2b0c151345f3feebeeab117b3
Elvan H <elvanhd@gmail.com>
2021-01-14 13:33:32 +0700

    adding the email and password fields

--- time_tracker_flutter_course/lib/app/sign_in/email_sign_in_form.dart
+++ time_tracker_flutter_course/lib/app/sign_in/email_sign_in_form.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 class EmailSignInForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _buildChildren() {
+    return [
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'user@example.com',
+        ),
+      ),
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      ),
+    ];
   }
 }
