dd58354efa3f794e4ca33e81c3b65458edf0572d
Elvan H <elvanhd@gmail.com>
2021-01-15 22:10:59 +0700

    updating the model

--- time_tracker_flutter_course/lib/app/auth/bloc/email_sign_in_bloc.dart
+++ time_tracker_flutter_course/lib/app/auth/bloc/email_sign_in_bloc.dart
@@ -1,13 +1,33 @@
 import 'dart:async';
 
+import '../model/email_sign_in_form_type.dart';
 import '../model/email_sign_in_model.dart';
 
 class EmailSignInBloc {
   final _modelController = StreamController<EmailSignInModel>();
+  EmailSignInModel _model = EmailSignInModel();
 
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
 
   void dispose() {
     _modelController.close();
+  }
+
+  void updateWith({
+    String email,
+    String password,
+    EmailSignInFormType formType,
+    bool isLoading,
+    bool submitted,
+  }) {
+    _model = _model.copyWith(
+      email: email,
+      password: password,
+      formType: formType,
+      isLoading: isLoading,
+      submitted: submitted,
+    );
+
+    _modelController.add(_model);
   }
 }
