dc07014d41a83eee447e579250009dd93ceccbb7
Elvan H <elvanhd@gmail.com>
2021-01-15 09:23:00 +0700

    creating an AuthProvider

--- time_tracker_flutter_course/lib/app/time_tracker_app.dart
+++ time_tracker_flutter_course/lib/app/time_tracker_app.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 import 'auth/auth_check.dart';
+import 'auth/auth_provider.dart';
 import 'auth/auth_service.dart';
 
 class TimeTrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Time Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: AuthCheck(
-        auth: AuthService(),
+    return AuthProvider(
+      auth: AuthService(),
+      child: MaterialApp(
+        title: 'Time Tracker',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: AuthCheck(
+          auth: AuthService(),
+        ),
       ),
     );
   }
