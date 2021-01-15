4f5609caa7742b50b12cfa59eb687a1d353f91a5
Elvan H <elvanhd@gmail.com>
2021-01-15 22:35:11 +0700

    addint the bloc submit method

--- time_tracker_flutter_course/lib/app/auth/bloc/email_sign_in_bloc.dart
+++ time_tracker_flutter_course/lib/app/auth/bloc/email_sign_in_bloc.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+
 import '../model/email_sign_in_form_type.dart';
 import '../model/email_sign_in_model.dart';
+import '../service/auth_base.dart';
 
 class EmailSignInBloc {
   final _modelController = StreamController<EmailSignInModel>();
   EmailSignInModel _model = EmailSignInModel();
+  final AuthBase auth;
+
+  EmailSignInBloc({@required this.auth});
 
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
 
@@ -29,5 +35,21 @@ class EmailSignInBloc {
     );
 
     _modelController.add(_model);
+  }
+
+  Future<void> submit() async {
+    updateWith(submitted: true, isLoading: true);
+
+    try {
+      if (_model.formType == EmailSignInFormType.signIn) {
+        await auth.signInWithEmailAndPassword(_model.email, _model.password);
+      } else {
+        await auth.createUserWithEmailAndPassword(
+            _model.email, _model.password);
+      }
+    } catch (exc) {
+      updateWith(isLoading: false);
+      rethrow;
+    }
   }
 }
