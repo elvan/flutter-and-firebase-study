89a95eeaede21d506359442ebe9cfef07316bf46
Elvan H <elvanhd@gmail.com>
2021-01-15 08:17:51 +0700

    dismissing a dialog

--- time_tracker_flutter_course/lib/app/sign_in/email_sign_in_form.dart
+++ time_tracker_flutter_course/lib/app/sign_in/email_sign_in_form.dart
@@ -134,7 +134,7 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
             content: Text(e.toString()),
             actions: [
               FlatButton(
-                onPressed: () {},
+                onPressed: () => Navigator.of(context).pop(),
                 child: Text('OK'),
               ),
             ],
