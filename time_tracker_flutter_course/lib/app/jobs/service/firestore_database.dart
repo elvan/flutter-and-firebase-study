472db1e8c307bcd389a5a512b4b16dee20850ed0
Elvan H <elvanhd@gmail.com>
2021-01-17 09:55:16 +0700

    adding a singleton FirestoreService class

--- time_tracker_flutter_course/lib/app/jobs/service/firestore_database.dart
+++ time_tracker_flutter_course/lib/app/jobs/service/firestore_database.dart
@@ -1,18 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
 import '../entity/job.dart';
 import 'api_path.dart';
 import 'database.dart';
+import 'firestore_service.dart';
 
 class FirestoreDatabase implements Database {
   final String uid;
+  final _service = FirestoreService.instance;
 
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
   @override
   Future<void> createJob(Job job) async {
-    _setData(
+    _service.setData(
       path: APIPath.job(uid, '2ksbtPLB7aHh7mn0M5k1'),
       data: job.toMap(),
     );
@@ -20,30 +21,9 @@ class FirestoreDatabase implements Database {
 
   @override
   Stream<List<Job>> jobsStream() {
-    return _collectionStream<Job>(
+    return _service.collectionStream<Job>(
       path: APIPath.jobList(uid),
       builder: (data) => Job.fromMap(data),
     );
-  }
-
-  Future<void> _setData({
-    String path,
-    Map<String, dynamic> data,
-  }) async {
-    final reference = FirebaseFirestore.instance.doc(path);
-    await reference.set(data);
-  }
-
-  Stream<List<T>> _collectionStream<T>({
-    @required String path,
-    @required T Function(Map<String, dynamic> data) builder,
-  }) {
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((querySnapshot) {
-      return querySnapshot.docs.map((documentSnapshot) {
-        return builder(documentSnapshot.data());
-      }).toList();
-    });
   }
 }
