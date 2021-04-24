b33a514a07fed1922492c6be19081547f6053e9c
Elvan H <elvanhd@gmail.com>
2021-04-24 15:58:20 +0700

    validating and saving form data

--- time_tracker_flutter_course/lib/app/jobs/view/add_job_page.dart
+++ time_tracker_flutter_course/lib/app/jobs/view/add_job_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AddJobPage extends StatefulWidget {
+  @override
+  _AddJobPageState createState() => _AddJobPageState();
+
   static Future<void> show(BuildContext context) async {
     await Navigator.of(context).push(
       MaterialPageRoute(
@@ -9,16 +12,29 @@ class AddJobPage extends StatefulWidget {
       ),
     );
   }
-
-  @override
-  _AddJobPageState createState() => _AddJobPageState();
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _name;
+  int _ratePerHour;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          FlatButton(
+            child: Text(
+              'Save',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: _submit,
+          )
+        ],
         elevation: 2.0,
         title: Text('New Job'),
       ),
@@ -47,6 +63,7 @@ class _AddJobPageState extends State<AddJobPage> {
         children: _buildFormChildren(),
         crossAxisAlignment: CrossAxisAlignment.stretch,
       ),
+      key: _formKey,
     );
   }
 
@@ -54,6 +71,8 @@ class _AddJobPageState extends State<AddJobPage> {
     return [
       TextFormField(
         decoration: InputDecoration(labelText: 'Job name'),
+        onSaved: (value) => _name = value,
+        validator: (value) => value.isNotEmpty ? null : "Name can't be empty",
       ),
       TextFormField(
         decoration: InputDecoration(labelText: 'Rate per hour'),
@@ -61,7 +80,24 @@ class _AddJobPageState extends State<AddJobPage> {
           decimal: false,
           signed: false,
         ),
+        onSaved: (value) => _ratePerHour = int.tryParse(value) ?? 0,
       ),
     ];
+  }
+
+  void _submit() {
+    if (_validateAndSaveForm()) {
+      print('Form was saved: $_name, rate per hour: $_ratePerHour');
+    }
+    // TODO: Submit data to firestore
+  }
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
   }
 }
