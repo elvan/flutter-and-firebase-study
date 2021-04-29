e4ce12bee69ec7cb661b7ced2f07d2a1bf2bac83
Elvan H <elvanhd@gmail.com>
2021-04-30 04:25:38 +0700

    the CupertinoPageRoute

--- time_tracker_flutter_course/lib/app/jobs/view/jobs_page.dart
+++ time_tracker_flutter_course/lib/app/jobs/view/jobs_page.dart
@@ -2,9 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../../common/show_alert_dialog.dart';
 import '../../../common/show_exception_alert_dialog.dart';
-import '../../auth/service/auth_base.dart';
 import '../../job_entries/job_entries_page.dart';
 import '../entity/job.dart';
 import '../service/database_service.dart';
