ff398d814d9bc86aada1ab3d9916c57f60043691
Elvan H <elvanhd@gmail.com>
2021-04-30 08:02:08 +0700

    testing an empty string

--- time_tracker_flutter_course/test/validator/email_and_password_validator_test.dart
+++ time_tracker_flutter_course/test/validator/email_and_password_validator_test.dart
@@ -2,9 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:time_tracker_flutter_course/validator/non_empty_string_validator.dart';
 
 void main() {
-  test('non empty string', () {
+  test('non empty string is valid', () {
     final validator = NonEmptyStringValidator();
 
     expect(validator.isValid('test'), true);
+  });
+
+  test('empty string is invalid', () {
+    final validator = NonEmptyStringValidator();
+
+    expect(validator.isValid(''), false);
   });
 }
