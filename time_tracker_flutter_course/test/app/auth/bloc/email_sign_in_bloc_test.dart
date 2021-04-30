52917fa1b64075c857dac6494502fa2914b9ee2c
Elvan H <elvanhd@gmail.com>
2021-05-01 06:23:48 +0700

    testing streams in blocs

--- time_tracker_flutter_course/test/app/auth/bloc/email_sign_in_bloc_test.dart
+++ time_tracker_flutter_course/test/app/auth/bloc/email_sign_in_bloc_test.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:time_tracker_flutter_course/app/auth/bloc/email_sign_in_bloc.dart';
 import 'package:time_tracker_flutter_course/app/auth/model/email_sign_in_model.dart';
 
@@ -22,6 +24,38 @@ void main() {
       'AND password is updated '
       'AND submit is called '
       'THEN modelStream emits the correct events', (tester) async {
-    expect(bloc.modelStream, emits(EmailSignInModel()));
+    when(mockAuth.signInWithEmailAndPassword(any, any))
+        .thenThrow(PlatformException(code: 'ERROR'));
+
+    expect(
+      bloc.modelStream,
+      emitsInOrder([
+        EmailSignInModel(),
+        EmailSignInModel(email: 'user@example.com'),
+        EmailSignInModel(
+          email: 'user@example.com',
+          password: 'pswd1234',
+        ),
+        EmailSignInModel(
+          email: 'user@example.com',
+          password: 'pswd1234',
+          submitted: true,
+          isLoading: true,
+        ),
+        EmailSignInModel(
+          email: 'user@example.com',
+          password: 'pswd1234',
+          submitted: true,
+          isLoading: false,
+        )
+      ]),
+    );
+
+    bloc.updateEmail('user@example.com');
+    bloc.updatePassword('pswd1234');
+
+    try {
+      await bloc.submit();
+    } catch (e) {}
   });
 }
