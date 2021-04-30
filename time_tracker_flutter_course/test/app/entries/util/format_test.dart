c13b4e066792cd47113a2d48e90b6abfeef46f37
Elvan H <elvanhd@gmail.com>
2021-04-30 19:39:11 +0700

    The setUp method and testing date formatting with locales

--- time_tracker_flutter_course/test/app/entries/util/format_test.dart
+++ time_tracker_flutter_course/test/app/entries/util/format_test.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:time_tracker_flutter_course/app/entries/util/format.dart';
 
 void main() {
@@ -17,6 +19,21 @@ void main() {
 
     test('decimal', () {
       expect(Format.hours(4.5), '4.5h');
+    });
+
+    group('date - GB local', () {
+      setUp(() async {
+        Intl.defaultLocale = 'en_GB';
+        await initializeDateFormatting(Intl.defaultLocale);
+      });
+
+      test('2019-08-12', () {
+        expect(Format.date(DateTime(2019, 8, 12)), '12 Aug 2019');
+      });
+
+      test('2019-12-30', () {
+        expect(Format.date(DateTime(2019, 12, 30)), '30 Dec 2019');
+      });
     });
   });
 }
