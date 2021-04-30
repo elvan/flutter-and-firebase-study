4bd2e4ca9d76f1a4c61d593ffd3f3401179104ae
Elvan H <elvanhd@gmail.com>
2021-04-30 20:20:02 +0700

    finding widgets and matcher arguments

--- time_tracker_flutter_course/test/common/custom_raised_button_test.dart
+++ time_tracker_flutter_course/test/common/custom_raised_button_test.dart
@@ -5,7 +5,15 @@ import 'package:time_tracker_flutter_course/common/custom_raised_button.dart';
 void main() {
   testWidgets('custom raised button ...', (tester) async {
     await tester.pumpWidget(MaterialApp(
-      home: CustomRaisedButton(),
+      home: CustomRaisedButton(
+        child: Text('Tap me'),
+      ),
     ));
+
+    final button = find.byType(RaisedButton);
+
+    expect(button, findsOneWidget);
+    expect(find.byType(FlatButton), findsNothing);
+    expect(find.text('Tap me'), findsOneWidget);
   });
 }
