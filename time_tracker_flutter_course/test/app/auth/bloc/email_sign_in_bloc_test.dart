2ad0212d8809662dddff8dfebadfa9d2eb2a8214
Elvan H <elvanhd@gmail.com>
2021-05-01 05:20:41 +0700

    testing blocs

---
+++ time_tracker_flutter_course/test/app/auth/bloc/email_sign_in_bloc_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:time_tracker_flutter_course/app/auth/bloc/email_sign_in_bloc.dart';
+import 'package:time_tracker_flutter_course/app/auth/model/email_sign_in_model.dart';
+
+import '../../../mocks.dart';
+
+void main() {
+  MockAuth mockAuth;
+  EmailSignInBloc bloc;
+
+  setUp(() {
+    mockAuth = MockAuth();
+    bloc = EmailSignInBloc(auth: mockAuth);
+  });
+
+  tearDown(() {
+    bloc.dispose();
+  });
+
+  testWidgets(
+      'WHEN email is updated '
+      'AND password is updated '
+      'AND submit is called '
+      'THEN modelStream emits the correct events', (tester) async {
+    expect(bloc.modelStream, emits(EmailSignInModel));
+  });
+}
